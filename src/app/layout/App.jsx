9f6facc94f9f1c69d919d87841a73e578cbe1801
marb61a <marb61a@gmail.com>
2018-06-26 17:18:48 +0100

    Added navbar and styling

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import { Button } from 'semantic-ui-react';
 
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render(){
     return(
       <div>
-        <h1>ReactEvents</h1>
+        <NavBar />
         <EventDashboard />
       </div>
     );
