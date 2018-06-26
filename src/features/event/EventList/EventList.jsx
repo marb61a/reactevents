f2e081977fb4d82d806530963ef2ea3e57eacbb2
marb61a <marb61a@gmail.com>
2018-06-26 17:27:50 +0100

    added eventlist folder and initial files

---
+++ src/features/event/EventList/EventList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EventList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>EventList</h1>
+      </div>
+    );
+  }
+}
+
+export default EventList;
