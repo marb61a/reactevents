6ff7e81b1a08f7473c460772ed714509a8d4969e
marb61a <marb61a@gmail.com>
2018-06-27 20:52:12 +0100

    updated EventForm

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 
 class EventForm extends Component {
+  state = {
+    event: {
+      title: ''
+    }
+  }
+
+  onFormSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(this.refs.title.value);
+  }
+
+  onTitleChange = (evt) => {
+    this.setState({
+      title: evt.target.value
+    })
+  }
+
   render() {
     const {handleCancel} = this.props;
+    const {event} = this.state;
 
     return (
       <Segment>
-        <Form>
+        <Form onSubmit={this.onFormSubmit}>
           <Form.Field>
             <label>Event Title</label>
-            <input placeholder="First Name" />
+            <input onChange={this.onTitleChange} value={event.title} placeholder="Event Title" />
           </Form.Field>
           <Form.Field>
             <label>Event Date</label>
