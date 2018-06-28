daeb34ff9fa9a9f331523bea3c2ebf96f825bf60
marb61a <marb61a@gmail.com>
2018-06-28 21:39:58 +0100

    updated home page and router configuration

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -7,13 +7,21 @@ class NavBar extends Component {
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
+          <Menu.Item as={Link} to="/" header>
             <img src="assets/logo.png" alt="logo" />
             ReactEvents
           </Menu.Item>
           <Menu.Item as={NavLink} to="/events" name="Events" />
+          <Menu.Item as={NavLink} to="/people" name="People" />
           <Menu.Item>
-            <Button floated="right" positive inverted content="Create Event" />
+            <Button
+              as={Link}
+              to="/createEvent"
+              floated="right" 
+              positive 
+              inverted 
+              content="Create Event" 
+            />
           </Menu.Item>
           <Menu.Item position="right">
             <Button basic inverted content="Login" />
