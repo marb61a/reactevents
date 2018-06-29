d6c09b11e9f2bcb1715a96fee9ce0e513eb57598
marb61a <marb61a@gmail.com>
2018-06-29 16:49:54 +0100

    updated homepage and navbar

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, withRouter } from 'react-router-dom';
 
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
@@ -20,6 +20,8 @@ class NavBar extends Component {
     this.setState({
       authenticated: false
     })
+
+    this.props.history.push('/');
   }
 
   render() {
@@ -33,7 +35,11 @@ class NavBar extends Component {
             ReactEvents
           </Menu.Item>
           <Menu.Item as={NavLink} to="/events" name="Events" />
-          <Menu.Item as={NavLink} to="/people" name="People" />
+          {
+            authenticated && 
+            <Menu.Item as={NavLink} to="/people" name="People" />
+          }
+          { authenticated &&
           <Menu.Item>
             <Button
               as={Link}
@@ -43,7 +49,7 @@ class NavBar extends Component {
               inverted 
               content="Create Event" 
             />
-          </Menu.Item>
+          </Menu.Item> }
           {authenticated 
             ? ( <SignedInMenu signOut={this.handleSignOut} /> )
             : ( <SignedOutMenu signIn={this.handleSignIn} /> )
@@ -55,4 +61,4 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
