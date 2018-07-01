e8620859a2622a63d17cdeb678aa060efbff93bc
marb61a <marb61a@gmail.com>
2018-07-01 21:43:18 +0100

    updated settings dashboard and nav

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Grid, GridColumn } from 'semantic-ui-react';
 
 const SettingsDashboard = () => {
     return (
-      <div>
-        <h1>Settings Dashboard Page</h1>
-      </div>
+      <Grid>
+        <Grid.Column width={12}>
+          <h1>Settings</h1>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <h1>Nav</h1>
+        </Grid.Column>
+      </Grid>
     );
 }
 
--- src/features/user/Settings/SettingsNav.jsx
+++ src/features/user/Settings/SettingsNav.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 const SettingsNav = () => {
     return (
-      <div>
-        <h1>Settings Nav Page</h1>
-      </div>
+      <Grid.Column width={4}>
+        <Menu vertical>
+          <Header icon="user" attached inverted color="grey" content="Profile" />
+          <Menu.Item>Basics</Menu.Item>
+          <Menu.Item>About Me</Menu.Item>
+          <Menu.Item>My Photos</Menu.Item>
+        </Menu>
+        <Grid.Row />
+        <Menu vertical>
+          <Header
+            icon="settings"
+            attached
+            inverted
+            color="grey"
+            content="Account"
+          />
+          <Menu.Item>My Account</Menu.Item>
+        </Menu>
+      </Grid.Column>
     );
 }
 
