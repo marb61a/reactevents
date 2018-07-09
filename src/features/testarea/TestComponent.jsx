2db6a4526ce3ca2613fedd4b157a134cceef438a
marb61a <marb61a@gmail.com>
2018-07-09 12:09:25 +0100

    added map to test component

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button } from 'semantic-ui-react';
+import GoogleMapReact from 'google-map-react';
+import { Button, Icon } from 'semantic-ui-react';
 import Script from 'react-load-script';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
@@ -15,9 +16,24 @@ const actions = {
   decrementCounter
 }
 
+const Marker = () => <Icon 
+  name="marker"
+  size="big"
+  color="red"
+/>;
+
 class TestComponent extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   state = {
-    address: ''
+    address: '',
+    scriptLoaded: false
   }
 
   handleScriptLoad = () => {
@@ -64,6 +80,19 @@ class TestComponent extends Component {
           }
           <button type="submit">Submit</button>
         </form>
+        <div style={{ height: '300px', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: '' }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <Marker
+              lat={59.955413}
+              lng={30.337844}
+              text={'Kreyser Avrora'}
+            />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
