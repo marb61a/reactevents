694df8631650be82962ce65c15d04af3e4536cb4
marb61a <marb61a@gmail.com>
2018-07-10 17:30:15 +0100

    setup asyncReducer and added to rootReducer

--- src/features/async/asyncReducer.jsx
+++ src/features/async/asyncReducer.jsx
@@ -8,3 +8,30 @@ import {
 const initialState = {
   loading: false
 };
+
+export const asyncActionStarted = (state) => {
+  return {
+    ...state,
+    loading: true
+  }
+}
+
+export const asyncActionFinished = (state) => {
+  return {
+    ...state,
+    loading: false
+  }
+}
+
+export const asyncActionError = (state) => {
+  return {
+    ...state,
+    loading: false
+  }
+}
+
+export default createReducer(initialState, {
+  [ASYNC_ACTION_START]: asyncActionStarted,
+  [ASYNC_ACTION_FINISH]: asyncActionFinished,
+  [ASYNC_ACTION_ERROR]: asyncActionError
+});
