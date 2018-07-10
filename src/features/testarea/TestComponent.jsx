945f78b655593dd09cbc58e065e21bff664bdba2
marb61a <marb61a@gmail.com>
2018-07-10 17:13:17 +0100

    updated testComponent and authActions

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -6,15 +6,18 @@ import Script from 'react-load-script';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 import { incrementAsync, decrementAsync } from './testActions';
+import { openModal } from '../modals/modalActions';
 
 const mapState = (state) => ({
-  data: state.test.data
+  data: state.test.data,
+  loading: state.test.loading
 });
 
 const actions = {
   incrementAsync,
-  decrementAsync
-}
+  decrementAsync,
+  openModal
+};
 
 const Marker = () => <Icon 
   name="marker"
@@ -59,7 +62,7 @@ class TestComponent extends Component {
       onChange: this.onChange,
     }
 
-    const { incrementCounter, decrementCounter, data } = this.props;
+    const { incrementAsync, decrementAsync, data, openModal, loading } = this.props;
 
     return (
       <div>
@@ -70,8 +73,13 @@ class TestComponent extends Component {
 
         <h1>Test Area</h1>
         <h3>The answer is : {data}</h3>
-        <Button onClick={incrementCounter} color='green' content='Increment'/>
-        <Button onClick={decrementCounter} color='red' content='Decrement'/>
+        <Button loading={loading} onClick={incrementAsync} color='green' content='Increment'/>
+        <Button loading={loading} onClick={decrementAsync} color='red' content='Decrement'/>
+        <Button 
+          onClick={() => openModal('Test Modal', {data: 43})}
+          color='teal'
+          content='Open Modal'
+        />
         <br/><br/>
 
         <form onSubmit={this.handleFormSubmit}>
