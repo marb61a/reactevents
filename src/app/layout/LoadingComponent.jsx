569b88455603dc704679de521a4e29459fda4bde
marb61a <marb61a@gmail.com>
2018-07-11 12:09:20 +0100

    added reduxtoastr

--- src/app/layout/LoadingComponent.jsx
+++ src/app/layout/LoadingComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimmer, Loader } from 'semantic-ui-react;'
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 const LoadingComponent = ({inverted}) => {
     return (
