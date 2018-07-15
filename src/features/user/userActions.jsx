4d5cd48039712b03de29837920fad1bdb2d51514
marb61a <marb61a@gmail.com>
2018-07-15 21:33:01 +0100

    adding upload images functionality to userActions

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -23,3 +23,36 @@ export const updateProfile = (user) => {
     }
   }
 };
+
+// Uploading images
+export const uploadProfileImage = (file, fileName) => {
+  return async(dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    // This is a synchronous method so no need to await
+    const user = firebase.auth().currentUser();
+    const path = `${user.uid}/user_images`;
+
+    try{
+      // Upload the image to firebase
+      let uploadedFile = await firebase.uploadFile(path, file, null, options);
+      
+      // Get image url
+      let downloadUrl = await uploadedFile.uploadTaskSnapshot.downloadUrl;
+
+      // Get userdoc
+      let userDoc = await firestore.get(`users/${user.uid}`);
+
+      // Check if there is an image url
+      if(!userDoc.data().photoUrl){
+        await firebase.updateProfile({
+          photoUrl: downloadUrl
+        });
+        
+      }
+
+    } catch(error){
+      console.log(error);
+    }
+  }
+};
