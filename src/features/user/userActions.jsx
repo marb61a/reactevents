9fb874af7bca8d713d5918a6d9e10084221d1c49
marb61a <marb61a@gmail.com>
2018-07-17 17:26:11 +0100

    added goingToEvent function to userActions

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -125,3 +125,36 @@ export const setMainPhoto = photo => {
     }
   };
 };
+
+export const goingToEvent = (event) => {
+  return async(dispatch, getState, {getFirestore}) => {
+    const firestore = getFirestore();
+    const user = firestore.auth().currentUser;
+    const photoURL = getState().firebase.profile.photoURL;
+    const attendee = {
+      going: true,
+      joinDate: Date.now(),
+      photoURL: photoURL || '/assets/user.png',
+      displayName: user.displayName,
+      host: false
+    };
+
+    try{
+      await firestore.update(`events/${event.id}`, {
+        [`attendees.${user.uid}`]: attendee
+      });
+
+      await firestore.set(`event_attendee/${event.id}_${user.uid}`, {
+        eventId: event.id,
+        userUid: user.uid,
+        eventDate: event.date,
+        host: false
+      });
+
+      toastr.success('Congratulations', 'You have signed up to th event');
+    } catch(error){
+      console.log(error);
+      toastr.error('Oops', 'There was a problem signing up to the event');
+    }
+  }
+};
