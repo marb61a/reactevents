84e76efd273236c426d217959fbd9c4961fdf378
marb61a <marb61a@gmail.com>
2018-07-18 13:02:09 +0100

    updated EventDashboard

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -22,7 +22,7 @@ class EventDashboard extends Component {
     moreEvents: false,
     loadingInitial: true,
     loadedEvents: []
-  }
+  };
 
   async componentDidMount(){
     let next = await this.props.getEventsForDashboard();
@@ -47,24 +47,41 @@ class EventDashboard extends Component {
 
   getNextEvents = async() => {
     const { events } = this.props;
-    
-  }
+    let lastEvent = events && events[events.length - 1];
+    console.log(lastEvent);
+
+    let next = await this.props.getEventsForDashboard(lastEvent);
+    console.log(next);
+
+    if(next && next.docs & next.docs.length <= 1){
+      this.setState({
+        moreEvents: false
+      });
+    }
+  };
 
   render() {
-    const { events, loading } = this.props;
+    const { loading } = this.props;
 
-    if(loading) return <LoadingComponent inverted={true} />
+    if(SVGAnimatedLengthList.state.loadingInitial) {
+      return <LoadingComponent inverted={true} />
+    };
 
     return (
       <Grid>
         <Grid.Column width={10}>
           <EventList 
-            deleteEvent={this.handleDeleteEvent}
-            events={events}
+            loading={loading}
+            moreEvents={moreEvents}
+            events={loadedEvents}
+            getNextEvents={this.getNextEvents}
            />
         </Grid.Column>
         <Grid.Column width={6}>
           <EventActivity />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Loader active={loading}/>
         </Grid.Column>
       </Grid>
     );
