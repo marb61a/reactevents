4b64ed22a8f00730bab17c606a1bdf9843accef4
marb61a <marb61a@gmail.com>
2018-07-19 17:25:13 +0100

    added new cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+exports.createActivity = functions.firestore.document('events/{eventId}')
+  .onCreate(event => {
+    let newEvent = event.data();
+    console.log(newEvent);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+    const activity = newActivity('newEvent', newEvent, event.id);
+    console.log(activity);
+
+    return admin
+      .firestore()
+      .collection('activity')
+      .add(activity)
+      .then(docRef => {
+        return console.log('Activity created with id: ', docRef.id);
+      })
+      .catch(err => {
+        return console.log('Error adding activity', err);
+      });
+  });
+  
--- src/features/event/EventDetailed/EventDetailedMap.jsx
+++ src/features/event/EventDetailed/EventDetailedMap.jsx
@@ -16,7 +16,7 @@ const EventDetailedMap = ({lat, lng}) => {
     <Segment attached='bottom' style={{padding: 0}}>
       <div style={{ height: '300px', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: '-' }}
+          bootstrapURLKeys={{ key: 'Y' }}
           defaultCenter={center}
           defaultZoom={zoom}
         >
