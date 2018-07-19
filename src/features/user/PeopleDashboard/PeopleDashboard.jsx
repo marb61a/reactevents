f6948aa3fc121a14ff7326992d3421a2745628a9
marb61a <marb61a@gmail.com>
2018-07-19 21:39:33 +0100

    updated PeopleDashboard

--- src/features/user/PeopleDashboard/PeopleDashboard.jsx
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -1,11 +1,39 @@
 import React  from 'react';
+import { Grid, Segment, Header, Card } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase'
+import PersonCard from './PersonCard';
+
+const query = ({auth}) => {
+    return [
+      {
+        collection: 'users',
+        doc: auth.uid,
+        subcollections: [{collection: 'following'}],
+        storeAs: 'following'
+      },
+      {
+        collection: 'users',
+        doc: auth.uid,
+        subcollections: [{collection: 'followers'}],
+        storeAs: 'followers'
+      }
+    ]
+};
+
+const mapState = state => ({
+  followings: state.firestore.ordered.following,
+  followers: state.firestore.ordered.followers,
+  auth: state.firebase.auth
+});
 
 const PeopleDashboard = () => {
-    return (
-      <div>
-        <h1>People Dashboard Page</h1>
-      </div>
-    );
-}
+  return (
+    <Grid>
+    
+    </Grid>
+  );
+} 
 
 export default PeopleDashboard;
